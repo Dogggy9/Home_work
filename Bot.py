c368355e9a6020ebfe5c1c59c6b644c5c7bf21e7
Dogggy9 <91497727+Dogggy9@users.noreply.github.com>
2022-10-21 21:49:48 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,18 +1,20 @@
 import telebot
 from telebot import types
-
-TOKEN = "5721389141:AAE3hEZfKPk5NsfbG-oDnIQF4XDYLH41IM8"
+from moduls.seting import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, """Я здороваюсь и рассказываю о возможностях
     /about_author, /get_keyboard, /get_keyboard_inline""")
 
+
 @bot.message_handler(commands=['about_author'])
 def about_author(message):
     bot.send_message(message.chat.id, "Кратко рассказываю о себе")
+
 
 @bot.message_handler(commands=['get_keyboard'])
 def get_keyboard(message):
@@ -20,9 +22,10 @@ def get_keyboard(message):
     key1 = types.KeyboardButton("1 кнопка")
     key2 = types.KeyboardButton("2 кнопка")
     key3 = types.KeyboardButton("3 кнопка")
-    key4 = types.KeyboardButton("4 кнопка")
+    key4 = types.KeyboardButton("/help")
     keyBoard.add(key1, key2, key3, key4)
     bot.send_message(message.chat.id, "возврат клавиатуры из 2-3 кнопок", reply_markup=keyBoard)
+
 
 @bot.message_handler(commands=['get_keyboard_inline'])
 def get_keyboard_inline(message):
@@ -34,7 +37,8 @@ def get_keyboard_inline(message):
     inlinekeyBoard.add(key1, key2, key3, key4)
     bot.send_message(message.chat.id, "возврат клавиатуры из 2-3 кнопок как сообщение", reply_markup=inlinekeyBoard)
 
-@bot.callback_query_handler(func=lambda m:True)
+
+@bot.callback_query_handler(func=lambda m: True)
 def inline(message):
     if message.data == "Кнопка 1":
         bot.send_message(message.message.chat.id, 'Ты нажал кнопку 1')
@@ -45,19 +49,21 @@ def inline(message):
     elif message.data == "Кнопка 4":
         bot.send_message(message.message.chat.id, 'Ты нажал кнопку 4')
 
+
 @bot.message_handler(content_types=['text'])
 def hi_user(message):
     if message.text == "1 кнопка":
         bot.send_message(message.chat.id, 'Ты нажал 1 кнопку')
     elif message.text == "2 кнопка":
         bot.send_message(message.chat.id, 'Ты нажал 2 кнопку')
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
     else:
         bot.send_message(message.chat.id, message.text)
+
 
 # @bot.message_handler(func=lambda m: True)
 # def echo_all(message):
 #     bot.send_message(message.chat.id, message.text)
 
 bot.infinity_polling()
-
-
